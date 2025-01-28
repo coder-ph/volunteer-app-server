90ca6b34e85d202d1d52b4c2cd0389d8d15eabc3
Phelix <phelixmbani@gmail.com>
2025-01-28 12:10:41 +0300

    create usermodel with validations

--- models.py
+++ models.py
@@ -1,19 +1,43 @@
-from models import db
-from flask_migrate import Migrate
-from flask import Flask, request, jsonify
-from flask_restful import Api, Resource
+from sqlalchemy.orm import validates
+from sqlalchemy_serializer import SerializerMixin
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+from sqlalchemy import Enum 
+from datetime import datetime
 
-app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///app.db'
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.json.compact = False
+metadata=MetaData()
+db = SQLAlchemy(metadata=metadata)
 
-migrate = Migrate(app, db)
-
-db.init_app(app)
-
-api = Api(app)
-
-
-if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, nullable=False, unique=True)
+    role = db.Column(db.Enum('organization', 'volunteer'), nullable=False) #organization or volunteer
+    created_at =  db.Column(db.DateTime, server_default = db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    
+    rsvps = db.relationship('Rsvp', back_populates='user', cascade ='all, delete-orphan', lazy='dynamic')
+    
+    @validates('role')
+    def validate_role(self, key, value):
+        roles = ['organization', 'volunteer']
+        if value in roles:
+            return value
+        else:
+            raise ValueError ('A user can either be organization or volunteer')
+    
+    @validates('username')
+    def validate_username(self, key, value):
+        if not isinstance(value, str):
+            raise ValueError ('username must be a string')
+        return value
+    
+    @validates('email')
+    def validate_email(self, key, value):
+        if '@' not in value:
+            raise ValueError ('Invalid email adress')
+        return value
+    def __repr__(self):
+        return f'<User: {self.id}, {self.username}, {self.email}>'
